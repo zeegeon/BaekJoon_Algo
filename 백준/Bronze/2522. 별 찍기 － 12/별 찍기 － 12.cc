ef5af5d173468fc6353cc437a4fe47a3e0a544d3
Ji <75569937+zeegeon@users.noreply.github.com>
2022-11-22 19:56:50 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <deque>
+using namespace std;
+
+int N;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> N;
+
+	for (int i = 1; i < N; ++i)
+	{
+		for (int a = N-i; a > 0; --a)
+		{
+			cout << " ";
+		}
+		for (int b = 0; b < i; ++b)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		cout << "*";
+	}
+	cout << "\n";
+
+	for (int i = 1; i < N; ++i)
+	{
+		for (int a = 0; a < i; ++a)
+		{
+			cout << " ";
+		}
+		for (int b = N-i; b > 0; --b)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
