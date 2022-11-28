9594386c2a991e4a15bab956faedd62dc06228ad
Ji <75569937+zeegeon@users.noreply.github.com>
2022-11-29 01:52:22 +0900

    [Bronze III] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -1,14 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
 	string a;
 	cin >> a;
 	int ans =0;
