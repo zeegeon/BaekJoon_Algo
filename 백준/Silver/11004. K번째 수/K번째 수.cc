de02ff71a1611b83d76a4b62ca9cdd88f53cff77
Ji <75569937+zeegeon@users.noreply.github.com>
2023-02-11 23:36:00 +0900

    [Silver V] Title: K번째 수, Time: 1056 ms, Memory: 41084 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+long long int arr[5000000];
+
+int main() 
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N, K;	
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + N);
+	
+	cout << arr[K - 1];
+
+	return 0;
+}
