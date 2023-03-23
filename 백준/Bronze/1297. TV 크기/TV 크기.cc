817c31627fcb7099a5bbfbf8d53ff74c8126655e
Ji <75569937+zeegeon@users.noreply.github.com>
2023-03-24 06:54:11 +0900

    [Bronze II] Title: TV 크기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/1297. TV 크기/TV 크기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int len,height,width;
+    
+    cin >> len >> height >> width;
+    
+    cout << (int)(len/sqrt(height*height+width*width)*height) << ' ' <<(int)(len/sqrt(height*height+width*width)*width);
+
+    return 0;
+}
